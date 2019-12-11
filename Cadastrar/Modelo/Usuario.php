362af85523bb97ffcc53a7cb0184c4e501b56062
unknown <victorxavier530@gmail.com>
2019-12-11 17:32:29 -0300

    Implementacao do cadastrar feito por Luiz Gustavo

---
+++ Cadastrar/Modelo/Usuario.php
@@ -0,0 +1,220 @@
+<?php
+require_once dirname(__FILE__) .'/../Dao/UsuarioDao.php';
+require_once dirname(__FILE__).'./Endereco.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Usuario
+ *
+ * @author PI OnYH
+ */
+class Usuario {
+    //put your code here
+    private $id;
+    private $nomeCompleto;
+    private $nomeUsuario;
+    private $email;
+    private $senha;
+    private $telefones;
+    private $endereco;
+    private $tipoUsuario;
+    private $servicos;
+    private $nomeEmpresa;
+    
+    function __construct() {
+        $this->telefones=array();
+    }
+
+    
+    function getId() {
+        return $this->id;
+    }
+    function getNomeEmpresa() {
+        return $this->nomeEmpresa;
+    }
+
+    function setNomeEmpresa($nomeEmpresa) {
+        $this->nomeEmpresa = $nomeEmpresa;
+    }
+
+    
+    function getNomeCompleto() {
+        return $this->nomeCompleto;
+    }
+
+    function getNomeUsuario() {
+        return $this->nomeUsuario;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getSenha() {
+        return $this->senha;
+    }
+
+    function getTelefone() {
+        return $this->telefones;
+    }
+
+    
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setNomeCompleto($nomeCompleto) {
+        $this->nomeCompleto = $nomeCompleto;
+    }
+
+    function setNomeUsuario($chamadoComo) {
+        $this->nomeUsuario = $chamadoComo;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setSenha($senha) {
+        $this->senha = $senha;
+    }
+
+    function setTelefone($telefone) {
+        if(is_array($telefone)){
+            $this->telefones=$telefone;
+        } else {
+            $this->telefones[] = $telefone;
+        }
+    }
+
+    function setEndereco($endereco) {
+        $this->endereco = $endereco;
+    }
+    function getTipoUsuario() {
+        return $this->tipoUsuario;
+    }
+
+    function setTipoUsuario($tipoUsuario) {
+        $this->tipoUsuario = $tipoUsuario;
+    }
+    function getServicos() {
+        return $this->servicos;
+    }
+
+    function setServicos($servicos) {
+        $this->servicos = $servicos;
+    }
+
+    function getEndereco() {
+        $e = Endereco::buscarPorId($this->id);
+        return "O endereço do(a) $this->nomeUsuario é: "
+                . "\n Pais: " . $e->getPais() .""
+                . "\n Estado: " . $e->getEstado(). ""
+                . "\n Cidade: " . $e->getCidade() . ""
+                . "\n Bairro: " . $e->getBairro() . ""
+                . "\n Logradouro: " . $e->getLogradouro(). ""
+                . "\n Número do estabelecimento: " . $e->getNumero();
+        
+    }
+//Metodo salvar 
+    public function salvar(){
+        return UsuarioDao::salvar($this->tipoUsuario, $this->nomeCompleto, $this->nomeUsuario, $this->email, $this->senha, $this->telefones
+                ,$this->servicos, $this->nomeEmpresa, '$this->enderecos');
+        
+    } 
+    public static function resgatarDados(){
+       $meuArray = array();
+       $dadosTabela = UsuarioDao::resgatarDados();
+      
+        if(is_object($dadosTabela)){
+            
+            while ($linha = $dadosTabela->fetch_assoc()){
+                $algumaCoisa = new Usuario();
+                $algumaCoisa->setEmail($linha["email"]);
+                //$algumaCoisa->setEndereco($linha["endereco"]);
+                $algumaCoisa->setId($linha["idUsuario"]);
+                $algumaCoisa->setNomeCompleto($linha["nomeCompleto"]);
+                $algumaCoisa->setNomeUsuario($linha["nomeUsuario"]);
+                $algumaCoisa->setSenha($linha["senha"]);
+//                $algumaCoisa->setTelefone($linha["telefone"]);
+                $algumaCoisa->setTipoUsuario($linha["tipoUsuario"]);
+                
+                $meuArray[] = $algumaCoisa;
+            }
+            
+        }
+        return $meuArray;
+    }
+    public static function login($email,$senha){
+        $dadosTabela = UsuarioDao::login($email, $senha);
+        //print_r($dadosTabela);
+        if(is_object($dadosTabela)){
+            while ($linha = $dadosTabela->fetch_assoc()){
+                $algumaCoisa = new Usuario();
+                $algumaCoisa->setEmail($linha["email"]);
+                //$algumaCoisa->setEndereco($linha["endereco"]);
+                $algumaCoisa->setId($linha["idUsuario"]);
+                $algumaCoisa->setNomeCompleto($linha["nomeCompleto"]);
+                $algumaCoisa->setNomeUsuario($linha["nomeUsuario"]);
+                $algumaCoisa->setSenha($linha["senha"]);
+//                $algumaCoisa->setTelefone($linha["telefone"]);
+                $algumaCoisa->setTipoUsuario($linha["tipoUsuario"]);
+                //print_r($algumaCoisa);
+                return $algumaCoisa;
+            }
+            
+        }
+        return "Email ou senha inválidos(as)";
+    }
+    public static function buscarUsuarioPorId($idUsuario){
+        $dadosTabela = UsuarioDao::buscarUsuarioPorId($idUsuario);
+        //print_r($dadosTabela);
+        if(is_object($dadosTabela)){
+            while ($linha = $dadosTabela->fetch_assoc()){
+                $algumaCoisa = new Usuario();
+                $algumaCoisa->setEmail($linha["email"]);
+                //$algumaCoisa->setEndereco($linha["endereco"]);
+                $algumaCoisa->setId($linha["idUsuario"]);
+                $algumaCoisa->setNomeCompleto($linha["nomeCompleto"]);
+                $algumaCoisa->setNomeUsuario($linha["nomeUsuario"]);
+                $algumaCoisa->setSenha($linha["senha"]);
+//                $algumaCoisa->setTelefone($linha["telefone"]);
+                $algumaCoisa->setTipoUsuario($linha["tipoUsuario"]);
+                $algumaCoisa->setNomeEmpresa($linha["nomeEmpresa"]);
+                //print_r($algumaCoisa);
+                return $algumaCoisa;
+            }
+            
+        }
+        return "id inválidos(as)";
+    }
+    
+    public static function buscarPorNomeEmpresa($nomeEmpresa){
+        $empresa = UsuarioDao::buscarPorNomeEmpresa($nomeEmpresa); 
+        $empresas = array();
+        if(is_object($empresa)){
+            while ($linha = $empresa->fetch_assoc()){
+                $algumaCoisa = new Usuario();
+                $algumaCoisa->setEmail($linha["email"]);
+                //$algumaCoisa->setEndereco($linha["endereco"]);
+                $algumaCoisa->setId($linha["idUsuario"]);
+                $algumaCoisa->setNomeCompleto($linha["nomeCompleto"]);
+                $algumaCoisa->setNomeUsuario($linha["nomeUsuario"]);
+                $algumaCoisa->setSenha($linha["senha"]);
+//                $algumaCoisa->setTelefone($linha["telefone"]);
+                $algumaCoisa->setNomeEmpresa($linha["nomeEmpresa"]);
+                $algumaCoisa->setTipoUsuario($linha["tipoUsuario"]);
+                //print_r($algumaCoisa);
+                $empresas[] = $algumaCoisa;
+            }
+            
+        }
+        return $empresas;
+    }
+    
+}
---
+++ Cadastrar/visao/telaCadastro.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>OnYH - Cadastre-se</title>
+        <?php
+        include_once './Include/css.php';
+        include_once './Include/js.php';
+        ?>
+
+
+    </head>
+
+    <body>
+        <?php
+        include_once './Include/cabecalho.php';
+        //print_r($_GET);
+        ?>
+
+        
+        <div class="divFormulario">
+            
+            
+            <fieldset class="fieldsetCentral">
+                <h1>Abra uma conta</h1>
+                <h2>É totalmente gratuito</h2>
+               
+                <form method="post" action="Funcoes/cadastrarUsuario.php">
+
+
+                    <div id="opcoes">
+                        
+                    
+                    <?php
+                    if (isset($_GET["tipoUsuario"])) {
+                        echo '<label for="rbtClienteComum">';
+                        echo '  Usuário cliente</label>';
+                        echo '<input onclick="onClickRadioButtons(this)" value="0" type="radio" name="tipoUsuario" id="rbtClienteComum" onChange="verificaNomeEmpresa(this)"> <br>';
+                        echo '<label for="rbtClienteEmpresario">';
+                        echo '  Usuário empreendedor</label>';
+                        echo'<input onclick="onClickRadioButtons(this)" value="1" type="radio" name="tipoUsuario" id="rbtClienteEmpresario" checked onChange="verificaNomeEmpresa(this)">';
+                    } else {
+                        echo '<label for="rbtClienteComum">';
+                        echo '  Usuário cliente</label>';
+                        echo'<input onclick="onClickRadioButtons(this)" value="0" type="radio" name="tipoUsuario" id="rbtClienteComum" checked  onChange="verificaNomeEmpresa(this)"><br>';
+                        echo '<label for="rbtClienteEmpresario">';
+                        echo '  Usuário empreendedor</label>';
+                        echo'<input onclick="onClickRadioButtons(this)" value="1" type="radio" name="tipoUsuario" id="rbtClienteEmpresario" onChange="verificaNomeEmpresa(this)">';
+                    }
+                    ?>
+                    </div>
+                  
+                    <div class="input-grupo">
+                    <label for="txtNomeCompleto" id="nomecomple">
+
+                        Nome completo:
+                    </label>
+                    <input type="text" id="txtNomeCompleto" autofocus name="txtNomeCompleto" placeholder="Nome completo">
+                    </div>
+                    
+                   
+                    <div class="input-grupo">
+                    <label for="txtChamado">
+                        Como gostaria de ser chamado:
+                    </label>
+                    <input type="text" id="txtChamado" name="txtChamado" placeholder="Nickname">
+                    </div>
+                    
+                    <div class="input-grupo">
+                    <label for="txtEmail">
+
+                        Email:
+                    </label>
+                    <input type="text" id="txtEmail" name="txtEmail" placeholder="Digite seu Email"> 
+                    </div>
+                    
+                    <div class="input-grupo">
+                    <label for="txtSenha">
+
+                        Senha:
+                    </label>
+                    <input type="password" id="txtSenha" name="txtSenha" placeholder="Digite sua senha">
+                    </div>
+                    
+                    
+                    <div class="input-grupo">
+                    <label for="txtConfirmarSenha">
+                        Confirmar Senha:
+                    </label>
+                    <input type="password" id="txtConfirmarSenha" name="txtConfirmarSenha" placeholder="Confirme sua senha">
+                    </div>
+                    
+                   
+                    
+                        
+                         <div class="input-grupo">
+                    
+                    <label for="txtPais">
+
+                        Pais:
+                    </label>
+                    <input type="text" id="txtPais" name="txtPais" placeholder="Digite seu país">
+                         </div>
+                         <div class="input-grupo">
+                     <label for="txtEstado">
+
+                        Estado:
+                    </label>
+                    <input type="text" id="txtEstado" name="txtEstado" placeholder="Digite seu estado">
+                         </div>
+                     <div class="input-grupo">
+                    <label for="txtCidade">
+
+                        Cidade:
+                    </label>
+                    
+                    <input type="text" id="txtCidade" name="txtCidade" placeholder="Digite sua cidade">
+                     </div>
+                     <div class="input-grupo">
+                    <label for="txtCep">
+
+                        CEP:
+                    </label>
+                    <input type="text" id="txtCep" name="txtCep" placeholder="Digite o CEP de sua cidade">
+                     </div>
+                     <div class="input-grupo">
+                    <label for="txtLogradouro">
+
+                        Logradouro:
+                    </label>
+                    <input type="text" id="txtLogradouro" name="txtLogradouro" placeholder="Digite seu Logradouro">
+                     </div>
+                     
+                     <div class="input-grupo">
+                    <label for="txtBairro">
+
+                        Bairro:
+                    </label>
+                    <input type="text" id="txtBairro" name="txtBairro" placeholder="Digite seu bairro">
+                     </div>
+                    
+                     <div class="input-grupo">
+                    <label for="txtNumero">
+
+                        Número:
+                    </label>
+                    <input type="text" id="txtNumero" name="txtNumero" placeholder="Digite o número">
+                    </div>
+                     
+                    
+                     
+                     
+                    
+                    <div class="input-grupo">
+                    <label for="txtNomeEmpresa" class="usuarioComum">
+                        Nome da Empresa:
+
+                    </label>
+                    <input type="text" id="txtNomeEmpresa" class="usuarioComum" name="txtNomeEmpresa" placeholder="Nome da Empresa">
+                    </div>
+                    
+ 
+                    <div class="input-grupo">
+                        
+                    
+                        <input type="button" id="btnTelefone" onclick="addTelefone(this)"value="Adicionar Tel" class="botoes" >
+                   </div>
+                   
+                    <div class="input-grupo">
+                    <label for="SelecaoServicos" class="usuarioComum" id="servicodisp">
+
+                        Selecione quais serviços você dispõe:
+                    </label>
+                    </div> 
+                    
+                       <?php
+                        include_once './Include/listaDeNegocios.php';
+                        ?>
+                    <div id="divServicos"> 
+                        
+                        
+                        <input type="button" id="btnServico" value="Adicionar Serviço" onclick="addNovoServico(this)" id ="addservico" class="botoes">
+                       
+                        
+                    </div>
+                    
+                    <div class="divBotoes">
+                        <input type="submit" value="Cadastrar" class="botoes">
+                    </div>
+                </form>
+            </fieldset>
+        
+        </div>
+
+        <?php
+        if (isset($_SESSION['msg'])) {
+            echo '<script> onclick("myModal") </script>';
+            unset($_SESSION['msg']);
+            unset($_SESSION['msg2']);
+        }
+
+        include_once './Include/rodape.php';
+        ?>
+        
+    </body>
+</html>
